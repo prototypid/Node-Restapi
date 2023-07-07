e66c7182fc230da5c1492bb60b637636812cbcef
Sahil Sharma <prototypid@gmail.com>
2023-07-07 11:29:53 +0530

    Add post route to create a temp post

--- controllers/feed.js
+++ controllers/feed.js
@@ -8,3 +8,13 @@ exports.getPosts = (req, res, next) => {
     ],
   });
 };
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
