969f0792afd84764f3faef1f82c4170e522f9ecf
Sahil Sharma <prototypid@gmail.com>
2023-07-07 12:54:01 +0530

    Create new post after validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -16,18 +17,28 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
 
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "test/img",
+    creator: { name: "Thatsboy" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => console.log(err));
 };
