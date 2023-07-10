39d2b50a18af47cd8f8b9f5ab33fc9a5eef4fe63
Sahil Sharma <prototypid@gmail.com>
2023-07-10 14:38:30 +0530

    Add stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,7 @@
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
+
 const authMiddleware = require("../middleware/is-auth");
 
 describe("Auth middleware", function () {
@@ -22,5 +25,32 @@ describe("Auth middleware", function () {
     };
 
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("Should throw an error if the token verification fails", function () {
+    const req = {
+      get: function () {
+        return "Bearer xyz";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("Should yield a userId after decoding the token", function () {
+    const req = {
+      get: function () {
+        return "Bearer fjsojfeoshoh";
+      },
+    };
+
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+
+    authMiddleware(req, {}, () => {});
+
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
   });
 });
