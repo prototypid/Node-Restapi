4ffb59ed066136c583ae02c00e4b93f863f98e30
Sahil Sharma <prototypid@gmail.com>
2023-07-11 15:02:43 +0530

    Add route to update user status

--- routes/auth.js
+++ routes/auth.js
@@ -34,4 +34,11 @@ router.post("/login", authController.login);
 
 router.get("/status", isAuth, authController.getUserStatus);
 
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
+
 module.exports = router;
