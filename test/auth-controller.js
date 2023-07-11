adba5e2500ff19ff1cf1e876ea7da39ce4114717
Sahil Sharma <prototypid@gmail.com>
2023-07-11 15:05:44 +0530

    Test get user status

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,7 +1,12 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
+
+const config = require("../utils/config");
 const User = require("../models/user");
 const AuthControllers = require("../controllers/auth");
+
+const MONGODB_URI = `mongodb://${config.database_username}:${config.database_password}@${config.database_host}:${config.database_port}/${config.test_database_name}?authSource=admin&w=1`;
 
 describe("Auth Controller - Login", function () {
   // Test connection to database fails
@@ -29,3 +34,41 @@ describe("Auth Controller - Login", function () {
     User.findOne.restore();
   });
 
+  it("should send a response with a valid user status for an existing user", function (done) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(() => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "64b7a53b4f386a39c5cef635",
+        });
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: "64b7a53b4f386a39c5cef635" };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+
+        // running our code
+        AuthControllers.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("New");
+          done();
+        });
+      })
+
+      .catch((err) => console.log(err));
+  });
+});
